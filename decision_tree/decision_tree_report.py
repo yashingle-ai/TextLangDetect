f1217d9efee84c25f15d97ada31909f90237d4da
Yash Ingle <yash.ingle003@gmail.com>
2025-01-28 23:51:55 +0530

    Create decision_tree_report.py

---
+++ decision_tree/decision_tree_report.py
@@ -0,0 +1,52 @@
+Test Set Classification Report:
+              precision    recall  f1-score   support
+
+         asm       0.92      0.73      0.81        62
+         ben       0.98      0.90      0.94        90
+         eng       1.00      0.91      0.95        90
+         guj       1.00      0.90      0.95       146
+         hin       1.00      0.94      0.97        94
+         mal       0.63      1.00      0.77       197
+         ori       1.00      0.84      0.91        73
+         pan       1.00      0.87      0.93       102
+         tam       1.00      0.84      0.91       108
+         tel       0.99      0.92      0.96       210
+         urd       1.00      0.89      0.94        73
+
+    accuracy                           0.90      1245
+   macro avg       0.96      0.88      0.91      1245
+weighted avg       0.94      0.90      0.91      1245
+
+
+Dev Set Classification Report:
+              precision    recall  f1-score   support
+
+         asm       0.90      0.62      0.74        61
+         ben       0.98      0.91      0.94        89
+         eng       0.99      0.93      0.96        90
+         guj       1.00      0.87      0.93       145
+         hin       1.00      0.87      0.93        93
+         mal       0.59      1.00      0.74       196
+         ori       1.00      0.90      0.95        72
+         pan       1.00      0.82      0.90       102
+         tam       1.00      0.85      0.92       107
+         tel       1.00      0.89      0.94       209
+         urd       1.00      0.86      0.93        72
+
+    accuracy                           0.89      1236
+   macro avg       0.95      0.87      0.90      1236
+weighted avg       0.93      0.89      0.89      1236
+
+Evaluation Metrics (Macro, Micro, Weighted):
+
+Macro Averaging:
+Dev Precision: 0.951004166222102 Dev Recall: 0.8667425013832871 Dev F1-Score: 0.8984499511704787
+Test Precision: 0.9564115071483278 Test Recall: 0.8845967889064457 Test F1-Score: 0.9129178033048916
+
+Micro Averaging:
+Dev Precision: 0.8851132686084142 Dev Recall: 0.8851132686084142 Dev F1-Score: 0.8851132686084142
+Test Precision: 0.9020080321285141 Test Recall: 0.9020080321285141 Test F1-Score: 0.9020080321285141
+
+Weighted Averaging:
+Dev Precision: 0.9280319557512432 Dev Recall: 0.8851132686084142 Dev F1-Score: 0.8937764543930302
+Test Precision: 0.9350003591143978 Test Recall: 0.9020080321285141 Test F1-Score: 0.9088784169429589
